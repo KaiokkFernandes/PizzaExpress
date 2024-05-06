620314f9c3af48ea8f36a9875fc6b65c8b11b009
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-05-06 11:28:26 -0300

    Corrigir rota de teste e adicionar dependências de CORS e express-async-errors

--- Back/src/routes.ts
+++ Back/src/routes.ts
@@ -3,8 +3,8 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response)=> {
-     return res.json({message: "Hello World!"})
+    throw new Error('Error ao fazer a requisição!');
 })
 
-
 export {router};
+
--- Back/src/server.ts
+++ Back/src/server.ts
@@ -1,8 +1,24 @@
 import express, {request, Response, NextFunction, Router} from 'express';
+import 'express-async-errors'
+import { router } from './routes'; 
+import cors from 'cors';   
 
-const app = express();  
+const app = express();
+app.use(express.json());
+app.use(cors());     
+app.use(router);
 
-app.use(Router);
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if(err instanceof Error){
+        return res.status(400).json({ 
+            error: err.message
+        });
+    }
+    return res.status(500).json({
+        status: "error",  
+        message: "Internal Server Error"        
+    });
 
+});  
 app.listen(3333, () => console.log('Server is running!'));
 
