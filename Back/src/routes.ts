b47732cfc2961ace0d71864c1512044e985b5ea4
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-05-09 01:13:25 -0300

    feat: Adicionar rota de criação de usuário
    Adiciona a rota de criação de usuário no arquivo de rotas. Agora é possível criar um novo usuário através do endpoint '/users'.

--- Back/src/routes.ts
+++ Back/src/routes.ts
@@ -1,10 +1,10 @@
 import { Router, Request, Response } from "express";
+import {CreateUserController} from './controllers/user/createUserController';  
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response)=> {
-    throw new Error('Error ao fazer a requisição!');
-})
+/* Rotas de Users*/
+router.post('/users', new CreateUserController().handle)
 
 export {router};
 
