269e52022c04786e52666215556f91317c645518
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-05-09 22:29:52 -0300

    Criando endpoint para criar user adicionando select para que não retorne a senha do usuario

--- Back/src/services/user/createUserService.ts
+++ Back/src/services/user/createUserService.ts
@@ -1,5 +1,4 @@
-
-
+import prismaClient from "../../prisma";
 interface UserRequest{
     name: string;
     email: string;
@@ -10,8 +9,36 @@ interface UserRequest{
 class CreateUserService{
     async execute({name, email, password}: UserRequest){
 
-        console.log(name);  
-        return {ok: true}
+        //Verificar se o usuário  enviou o email,
+
+        if(!email){
+            throw new Error("Email incorrect")
+
+        }
+        // Verificar se o email esta cadastrado na plataforma 
+         const  userAlreadyExists =  await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+         })
+         if(userAlreadyExists){
+            throw new Error("User already exists")  
+         }
+        const user = await prismaClient.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: password ,
+            },
+            select:{
+            id: true,
+            name: true,
+            email: true,
+            }
+        })
+ 
+       
+        return user;
     }
 }
 
