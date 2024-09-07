055239629d718580f7bb16db10e1328d672c58da
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-09-06 23:50:17 -0300

    screen login

--- Front/pizza-front/src/app/page.tsx
+++ Front/pizza-front/src/app/page.tsx
@@ -1,21 +1,45 @@
+ "use client";
 import Image from "next/image";
 import styles from "./page.module.scss";
 import logo from "/public/logo.svg";
 import Link from "next/link";
-
+import { api } from "@/services/api";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  
+   async function handleLogin(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;  
+    const password = formData.get("password") as string;  
+
+    if(email === "" || password === ""){ 
+      return;
+    }
+    try {
+      const response = await api.post("/session", {
+        email,
+        password
+      });
+    } catch {
+      alert("Erro ao logar");
+      return;
+    };
+    redirect("/dashboard");
+   }
+
   return (
     <>
     <div className={styles.containerCenter}>
       <Image src={logo} alt={"logo2"}></Image>
 
       <section className={styles.login}>
-        <form>
+        <form onSubmit={handleLogin}>
           <input
           type="email"
           required
-          name="email:"
+          name="email"
           placeholder="Digite seu email"  
           className={styles.input}
           />
@@ -27,9 +51,9 @@ export default function Home() {
           className={styles.input}  
           />
           <button type="submit"
-
+           className={styles.button}
           >Acessar</button>
-          <Link href="/register"> 
+          <Link href="/signup"> 
           Não possui uma conta? Cadastre-se
           </Link>
         </form>
