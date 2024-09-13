767656114541c73c3e513c392af566f88f430fb2
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-09-13 09:05:10 -0300

    feat: Add dependencies for cookies, lucide-react, sonner, and zod
    This commit adds the necessary dependencies for cookies-next, lucide-react, sonner, and zod to the package.json and package-lock.json files. These dependencies will be used in the project for handling cookies, rendering icons, and form validation.

--- Front/pizza-front/src/app/page.tsx
+++ Front/pizza-front/src/app/page.tsx
@@ -1,76 +1,85 @@
- "use client";
-import Image from "next/image";
-import styles from "./page.module.scss";
-import logo from "/public/logo.svg";
-import Link from "next/link";
-import { api } from "@/services/api";
+import Image from 'next/image';
+import styles from './page.module.scss';
+
+import logoImg from '/public/logo.svg';
+
+import Link from 'next/link';
+import { cookies } from 'next/headers';
 import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
+import { api } from '@/services/api';
 
-export default function Home() {
-  
-   async function handleLogin(event: React.FormEvent<HTMLFormElement>){
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const email = formData.get("email") as string;  
-    const password = formData.get("password") as string;  
+export default function Page() {
 
-    if(email === "" || password === ""){ 
+  async function handleLogin(formData: FormData){
+    "use server"
+    
+    const email = formData.get("email")
+    const password = formData.get("password")
+
+    if(email === '' || password === ''){
       return;
     }
-    try {
-      const response = await api.post("/session", {
-        email,
-        password
-      });
-       cookies().set("session", response.data.token, {
-      maxAge: 60 * 60 * 24 * 7, 
-      path: "/",
-      httpOnly: false,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict"  
-        
-       });
-    } catch {
-      alert("Erro ao logar");
-      return;
-    };
-    redirect("/dashboard");
-    
 
-   
-   }
+    try{
+      const response = await api.post("/session", {
+        email: email,
+        password: password,
+      })
+  
+      if(!response.data.token){
+        return;
+      }
+  
+      const expiresTime = 60 * 60 * 24 * 30 * 1000;
+      cookies().set("session", response.data.token,
+        {
+          maxAge: expiresTime,
+          httpOnly: false,
+          path: "/",
+          secure: process.env.NODE_ENV === "production",
+        });
+  
+    }catch(err){
+      console.log(err);
+    }
+
+    redirect("/dashboard")
+  }
 
   return (
     <>
     <div className={styles.containerCenter}>
-      <Image src={logo} alt={"logo2"}></Image>
+      <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
 
-      <section className={styles.login}>
-        <form onSubmit={handleLogin}>
+      <div className={styles.login}>
+        <form action={handleLogin} >
           <input
-          type="email"
-          required
-          name="email"
-          placeholder="Digite seu email"  
-          className={styles.input}
+            placeholder="Digite seu email"
+            className={styles.input}
+            type="email"
+            name="email"
+            required
           />
+
           <input
-          type="password" 
-          required  
-          name="password" 
-          placeholder="Digite sua senha"  
-          className={styles.input}  
+            placeholder="Digite sua senha..."
+            className={styles.input}
+            type="password"
+            name="password"
+            required
           />
-          <button type="submit"
-           className={styles.button}
-          >Acessar</button>
-          <Link href="/signup"> 
-          Não possui uma conta? Cadastre-se
-          </Link>
+          
+          <button type="submit" className={styles.button}>
+            Acessar
+          </button>
         </form>
-      </section>
+        
+        <Link href="/signup" className={styles.text}>
+           Nao possui uma conta? Cadastre-se
+        </Link>
+
       </div>
+    </div>
     </>
-  );
+  )
 }
